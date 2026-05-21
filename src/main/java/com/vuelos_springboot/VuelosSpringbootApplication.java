21c2072eb156d9673807a66402c523483fbbaa33
Franco <chirino.franco@uncuyo.edu.ar>
2026-05-20 23:32:22 -0300

    Primer commit

---
+++ src/main/java/com/vuelos_springboot/VuelosSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.vuelos_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuelosSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VuelosSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vuelos_springboot/VuelosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuelos_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuelosSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
